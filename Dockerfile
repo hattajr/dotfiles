4e197ba538da6923f7040b68f1af74e7f18b0cd1
hattajr <atadonjep@gmail.com>
2022-08-02 14:35:51 +0900

    nvim v.0.8

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 # ubuntu 20.04
 FROM ubuntu:20.04
-
-WORKDIR $HOME
-
+ENV TZ=Asia/Seoul
+ENV XDG_CONFIG_HOME=/home
+RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+WORKDIR /home/
 COPY . .
-
 RUN chmod +x install.sh && ./install.sh
-CMD ["sh", "-c", "echo Hello dot"]
+CMD ["sh", "-c", "zsh"]
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 echo "Prepare for installation..."
 
@@ -8,17 +8,6 @@ apt-get update
 
 #install curl
 apt-get install curl -y
-# isntall zsh 
-apt-get install zsh -y && zsh --version
-# install oh-my-zsh & powerlevel10k
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-
-# install exa : color full ls
-apt-get install -y exa
-# for ubuntu < 20.01
-# wget -c http://old-releases.ubuntu.com/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb
-# sudo apt-get install ./exa_0.9.0-4_amd64.deb 
 
 # install bat
 apt-get install -y bat
@@ -27,9 +16,22 @@ apt-get install -y bat
 apt-get install -y git
 
 # install neovim 0.5
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-apt-get install -y neovim
+apt-get install -y software-properties-common \
+  && add-apt-repository ppa:neovim-ppa/unstable \
+  && apt-get update \
+  && apt-get install -y neovim
+
+# isntall zsh 
+apt-get install zsh -y && zsh --version
+# install oh-my-zsh & powerlevel10k
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+# install exa : color full ls
+#apt-get install -y exa
+# for ubuntu < 20.01
+# wget -c http://old-releases.ubuntu.com/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb
+# sudo apt-get install ./exa_0.9.0-4_amd64.deb 
 
 # install vim-plug
 

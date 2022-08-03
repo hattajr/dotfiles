c1d10ccbed33a6c113ab5cb4c4c52bcc2473926a
hattajr <atadonjep@gmail.com>
2022-08-03 11:30:50 +0900

    initial cicit

---
+++ symlink.sh
@@ -0,0 +1,6 @@
+ln -snf ~/.dotfiles/.config ~/
+ln -s -f ~/.dotfiles/.gitconfig ~/
+ln -s -f ~/.dotfiles/.bashrc ~/
+ln -s -f ~/.dotfiles/.profile ~/
+ln -s -f ~/.dotfiles/.zshrc ~/
+ln -s -f ~/.dotfiles/.p10k.zsh ~/

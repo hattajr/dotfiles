11fc5e52d609777d39cc922d3ddf62086c9fec8f
hattajr <53370870+hattajr@users.noreply.github.com>
2024-02-05 08:25:13 +0900

    Ready. Set. Go!

--- letsgo.sh
+++ letsgo.sh
@@ -7,7 +7,7 @@ install_apt() {
     if [ -x "$(command -v $app_name)" ]; then
         echo \> Found $($app_name --version)
     else
-        echo \> '$app_name' not found. Installing...
+        echo \> "'$app_name' not found. Installing..."
         # Install the app
         sudo apt-get update
         sudo apt-get install -y "$app_name"
@@ -22,21 +22,22 @@ install_apt() {
 
 }
 
-install_apt "software-properties-common"
+# install_apt "software-properties-common"
 
-install_apt "git"
+# install_apt "git"
 
-install_apt "curl"
+# install_apt "curl"
 
-install_apt "zsh"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# # ZSH
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-install_apt "cargo"
+# install_apt "cargo"
 install_apt "stow"
 
 # APT install
-apt install tmux
+# apt install tmux
 
 
 # Install Neovim
@@ -56,15 +57,5 @@ else
         echo \> Error: $app_name installation is failed!
     fi
 fi
-
-
-
-
-
-# install exa : color full ls
-#apt-get install -y exa
-# for ubuntu < 20.01
-# wget -c http://old-releases.ubuntu.com/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb
-# sudo apt-get install ./exa_0.9.0-4_amd64.deb 
 
 # install vim-plug
